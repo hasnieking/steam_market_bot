79cf945676ac1de1a5d6dc1666cc29f62382165e
Jay Jonkman <jayjonk@gmail.com>
2022-08-17 01:50:47 +0200

    prints lowest price of all items in urls.txt

--- fetchPrice.py
+++ fetchPrice.py
@@ -1,15 +1,32 @@
+import json
+from xml.etree.ElementTree import tostring
 import requests
+import json
 
-#def get()
+#The given url is in the form of:
+#https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20M4A1-S%20%7C%20Hyper%20Beast%20%28Minimal%20Wear%29
+#This code changes it to the form:
+#https://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=StatTrak%E2%84%A2%20M4A1-S%20%7C%20Hyper%20Beast%20%28Minimal%20Wear%29
+#and then returns the fetched data from the market
 
 
-#def getAppID(url):
-#    code!
+offset = 43
+jsonurl = ["https://steamcommunity.com/market/priceoverview/?appid=", "&currency=", "&market_hash_name="]
+
+#convert url
+def getFetchURL(url, currency):
+    shortened = url[offset:]
+    split = shortened.split('/')
+    appid = split[0]
+    market_hash_name = split[1]
+    newurl = jsonurl[0] + appid + jsonurl[1] + str(currency) + jsonurl[2] + market_hash_name
+    
+    return newurl
 
 
-
-
-def getNums(url):
-    #id = getAppID(url)
-    r = requests.get(url)
-    print(r.text)
+#get data from Steam servers
+def fetch(url, currency):
+    fetchurl = getFetchURL(url, currency)
+    r = requests.get(fetchurl)
+    data = json.loads(r.text)
+    return data
--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
+from cgi import print_exception
 import fetchPrice
+import re
+import time
 
 
 filename = "urls.txt"
+currency = 3 #USD = 1, GPB = 2, EUR = 3, etc
+
+
+#start
+def start():
+    getURLS()
+
+    for url in urls:
+        data = fetchPrice.fetch(url, currency)
+        print(getLowestPrice(data))
+        time.sleep(1)
 
 
 
 
+#get lowest price from fetched data
+def getLowestPrice(data):
+    price_str = data["lowest_price"]
+    price_str = re.sub(r'[^0-9]', '', price_str)
+    return int(price_str)
+
+
+
+#get urls from file
 urls = []
-
-
 def getURLS():
     f = open(filename, "r")
     lines = f.readlines()
@@ -20,5 +41,8 @@ def getURLS():
         line = line.replace("http:", "https:")
         urls.append(line)
 
+    return
 
-getURLS()
+
+
+start()
